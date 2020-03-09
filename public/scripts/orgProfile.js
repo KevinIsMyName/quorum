41eb8831d387b8df392264c63d8e55e1e823fdc5
Vi <vickyviwing@gmail.com>
2020-03-09 12:12:32 -0400

    Created scripts for organization side

---
+++ public/scripts/orgProfile.js
@@ -0,0 +1,18 @@
+//Info:
+//1. Name of Student Organization
+//2. About
+//3. Email
+//4. Phone Number 
+
+/*
++-----------+--------------+------+-----+---------+----------------+
+| Field     | Type         | Null | Key | Default | Extra          |
++-----------+--------------+------+-----+---------+----------------+
+| orgID     | int          | NO   | PRI | NULL    | auto_increment |
+| orgName   | varchar(255) | NO   |     | NULL    |                |
+| about     | text         | YES  |     | NULL    |                |
+| orgEmail  | varchar(40)  | NO   |     | NULL    |                |
+| orgNumber | varchar(20)  | NO   |     | NULL    |                |
++-----------+--------------+------+-----+---------+----------------+
+*/
+
