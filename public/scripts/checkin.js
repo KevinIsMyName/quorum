586546a050f8f8f3a97e017dad17c45679b7a22d
KevinIsMyName <wu.kevin.2520@gmail.com>
2020-03-18 19:31:21 -0400

    Numeric code only

--- public/scripts/checkin.js
+++ public/scripts/checkin.js
@@ -2,6 +2,12 @@ const checkBtn = document.getElementById("checkBtn");
 var errormsg = document.getElementById("errorMsg");
 let eventCode;
 
+// Code from https://stackoverflow.com/questions/5639346/what-is-the-shortest-function-for-reading-a-cookie-by-name-in-javascript
+function getCookieValue(a) {
+    var b = document.cookie.match('(^|[^;]+)\\s*' + a + '\\s*=\\s*([^;]+)');
+    return b ? b.pop() : '';
+}
+let userID = getCookieValue("userID") ? getCookieValue("userID") : "notLoggedIn";
 
 checkBtn.addEventListener("click", () => {
     eventCode = document.getElementById("eventCode").value;
@@ -10,13 +16,18 @@ checkBtn.addEventListener("click", () => {
         let data = this.response;
         if (data.error) {
             console.log("Error processing event code.");
-            errormsg.innerHTML = "Not a Valid Event Code. Please Try Again.";
+            errormsg.innerHTML = "Not a valid event code. Code must be numeric";
             errormsg.style.color = "red";
+            return -1;
         } else {
             console.log("Event code successfully processed.")
         }
     });
     xhr.responseType = "json";
-    xhr.open("GET", "checkedin?eventCode=" + eventCode);
+    let queryStr = "checkedin?eventCode=" + eventCode;
+    if (userID !== "notLoggedIn") {
+        queryStr += "&userID=" + userID;
+    }
+    xhr.open("GET", queryStr);
     xhr.send();
 });
