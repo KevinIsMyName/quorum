abc514bf4e2a6084b35975c9416f760786b32007
KevinIsMyName <wu.kevin.2520@gmail.com>
2020-03-18 18:51:47 -0400

    Must have cookie to load webpage. This cookie can be client-side written.

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -13,3 +13,21 @@ function newEvent() {
 function editOrgProfile() {
     window.location.assign("orgProfile.html");
 }
+
+// Code from https://stackoverflow.com/questions/5639346/what-is-the-shortest-function-for-reading-a-cookie-by-name-in-javascript
+function getCookieValue(a) {
+    var b = document.cookie.match('(^|[^;]+)\\s*' + a + '\\s*=\\s*([^;]+)');
+    return b ? b.pop() : '';
+}
+
+let userID;
+
+window.onload = (event) => {
+    let cookieString = document.cookie;
+    userID = getCookieValue("userID");
+    if (userID === "") {
+        window.location.assign("/");
+    }
+    console.log("User: " + userID);
+    // do user authentication, load the proper webpages
+};
