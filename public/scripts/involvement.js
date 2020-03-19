05f0c50bb1cdc6d05ea48381be0453e83f51cd39
KevinIsMyName <wu.kevin.2520@gmail.com>
2020-03-18 23:13:41 -0400

    Implemented parsing involved event details

--- public/scripts/involvement.js
+++ public/scripts/involvement.js
@@ -5,6 +5,8 @@ function getCookieValue(a) {
     var b = decodeURIComponent(document.cookie).match('(^|[^;]+)\\s*' + a + '\\s*=\\s*([^;]+)');
     return b ? b.pop() : '';
 }
+
+
 window.onload = (event) => {
     let userID = getCookieValue("userID");
     if (userID === "") {
@@ -12,9 +14,32 @@ window.onload = (event) => {
     }
     console.log("User: " + userID);
 
+    let output = document.getElementById("organizations");
+
     let xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", function() {
-        console.log(JSON.parse(this.response));
+    xhr.addEventListener("load", function () {
+        let data = JSON.parse(this.response);
+        if (data.length > 0) {
+            for (let i = 0; i < data.length; i++) {
+                let eventName = data[i].eventID;
+                let hostOrg = data[i].eventHostOrg;
+                let loc = data[i].location;
+
+                // extract date info
+                let d = new Date(data[i].date);
+                let monthNames = ["January", "February", "March", "April", "May", "June",
+                    "July", "August", "September", "October", "November", "December"];
+                let month = monthNames[d.getMonth()];
+                let date = d.getDate() + 1; // date values 1-12
+                let year = d.getFullYear();
+
+                let event = document.createElement("div");
+                event.innerHTML = eventName + " hosted by " + hostOrg + " on " + month + ", " + date + ", " + year;
+                output.append();
+            }
+        } else {
+            output.innerText = "Oops! Doesn't look like you've attended any events&#128546;";
+        }
     });
     let queryStr = "/involve?userID=" + userID;
     xhr.open("GET", queryStr);
